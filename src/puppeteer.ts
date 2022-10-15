378e5235ad20af91cf6b1fb299fd6d44eb5b793d
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-15 08:51:45 +0700

    reduce production size

--- src/puppeteer.ts
+++ src/puppeteer.ts
@@ -2,7 +2,7 @@ import { join } from 'path';
 
 const _args = `--type=renderer --no-sandbox --autoplay-policy=no-user-gesture-required --log-file=/media/dimaslanjaka/DATA/ProgramData/Otohits/OtohitsApp/logs/debug.log --field-trial-handle=6500724731146965368,15747076022479036310,131072 --disable-features=MimeHandlerViewInCrossProcessFrame --lang=en-US --locales-dir-path=/media/dimaslanjaka/DATA/ProgramData/Otohits/OtohitsApp/resources/locales --log-file=/media/dimaslanjaka/DATA/ProgramData/Otohits/OtohitsApp/logs/debug.log --log-severity=disable --resources-dir-path=/media/dimaslanjaka/DATA/ProgramData/Otohits/OtohitsApp/resources --user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:80.0) Gecko/20100101 Firefox/104.0.2 --num-raster-threads=2 --enable-main-frame-before-activation --service-request-channel-token=2011700786163168796 --renderer-client-id=4 --shared-files=v8_context_snapshot_data:100,v8_natives_data:101`;
 const ___args = [
-  '--user-data-dir=' + join(__dirname, 'tmp/puppeteer_profile'),
+  '--user-data-dir=' + join(process.cwd(), 'tmp/puppeteer_profile'),
   '--no-sandbox',
   '--disable-setuid-sandbox',
   '--disable-infobars',
@@ -14,7 +14,7 @@ const ___args = [
 ];
 
 export const defaultArg = [
-  '--user-data-dir=' + join(__dirname, 'tmp/puppeteer_profile'),
+  '--user-data-dir=' + join(process.cwd(), 'tmp/puppeteer_profile'),
   '--no-sandbox',
   '--disable-setuid-sandbox',
   '--disable-infobars',
